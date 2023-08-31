f0b1c40e49ae03e12a33f83bd438c17cbba338b3
Max <sultonbek.rakhimov@gmail.com>
2023-08-31 22:37:55 +0500

    Define post statistics service interface

---
+++ Training.Medium.Sandbox/DiscoverySection/Services/IPostStatisticsService.cs
@@ -0,0 +1,24 @@
+﻿using DiscoverySection.Models;
+
+namespace DiscoverySection.Services;
+
+public interface IPostStatisticsService
+{
+    /// <summary>
+    /// Returns query of trending posts
+    /// </summary>
+    /// <returns>Queryable source of posts</returns>
+    IQueryable<BlogPost> GetTrendingPosts();
+
+    /// <summary>
+    /// Returns query of viral posts
+    /// </summary>
+    /// <returns>Queryable source of posts</returns>
+    IQueryable<BlogPost> GetViralPosts();
+
+    /// <summary>
+    /// Returns query of popular posts
+    /// </summary>
+    /// <returns>Queryable source of posts</returns>
+    IQueryable<BlogPost> GetPopularPosts();
+}
