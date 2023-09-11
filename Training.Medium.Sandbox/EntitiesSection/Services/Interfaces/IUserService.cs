295c43a8e4f9cb6355ca194527e3d18887b6f8f7
Muhammadaziz Komiljonov <azizbek1820@gmail.com>
2023-09-11 22:45:08 +0500

    Added entities project

---
+++ Training.Medium.Sandbox/EntitiesSection/Services/Interfaces/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using
+using DiscoverySection.Models;
+
+namespace EntitiesSection.Services.Interfaces
+{
+    public interface IUserService
+    {
+        ValueTask<User> GetById(Guid Id)
+        {
+
+        }
+    }
+}
