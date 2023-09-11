dfdc387186d37d563947f38a58f1a77bfdf06b03
Abdurahmon <abdurahmonsadriddinov0412@gmail.com>
2023-09-11 22:31:38 +0500

    My summary

--- Training.Medium.Sandbox/DiscoverySection/Services/PostStaticsService.cs
+++ Training.Medium.Sandbox/DiscoverySection/Services/PostStaticsService.cs
@@ -12,17 +12,22 @@ namespace DiscoverySection.Services
         public IQueryable<BlogPost> GetPopularPosts()
         {
             throw new NotImplementedException();
+            //hozirgi popular bulayotgan bulayotgan postlar 
         }
 
         public IQueryable<BlogPost> GetTrendingPosts()
         {
             throw new NotImplementedException();
+            // trendga chiqayotgan postlar
         }
 
         public IQueryable<BlogPost> GetViralPosts()
         {
+            // qisqa vaqt ichida kutarilgan service
             throw new NotImplementedException();
         }
 
     }
+
+
 }
