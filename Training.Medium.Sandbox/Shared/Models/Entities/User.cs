f8278e8707c32f7442e498c99f0f0f702b0a7a69
Muhammadaziz Komiljonov <azizbek1820@gmail.com>
2023-09-11 22:36:29 +0500

    Update User model

--- Training.Medium.Sandbox/Shared/Models/Entities/User.cs
+++ Training.Medium.Sandbox/Shared/Models/Entities/User.cs
@@ -1,6 +1,23 @@
-﻿namespace Shared.Models.Entities;
+﻿using Shared.Models.Common;
 
-public class User
+namespace Shared.Models.Entities;
+
+public class User : SoftDeletedEntity
 {
-    
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string EmailAddress { get; set; }
+
+    public User(string FirstName, string LastName, string EmailAddress)
+    {
+        Id = Guid.NewGuid();
+        base.IsDeleted = false;
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+        this.EmailAddress = EmailAddress;
+    }
+
+
+
 }
