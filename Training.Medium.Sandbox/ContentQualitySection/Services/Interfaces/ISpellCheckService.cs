54d4527ab936ee05632ba129a2bbba69ff17b1e6
Abdura <abdura52.uz@gmail.com>
2023-09-13 09:21:39 -0700

    Spellcheck service added

---
+++ Training.Medium.Sandbox/ContentQualitySection/Services/Interfaces/ISpellCheckService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentQualitySection.Services.Interfaces
+{
+    public interface ISpellCheckService
+    {
+        /// <summary>
+        /// Gets the words from the content
+        /// </summary>
+        /// <param name="content">The content to split to words</param>
+        /// <returns>Words list</returns>
+        IList<string> GetWords(string content);
+
+        /// <summary>
+        /// Gets the splitted word from the content
+        /// </summary>
+        /// <param name="content">The content to split to words</param>
+        /// <returns></returns>
+        IList<string> GetSplitWords(string content);
+
+        /// <summary>
+        /// Gets the sentences from the content
+        /// </summary>
+        /// <param name="content">The content to split to sentences</param>
+        /// <returns>Sentences list</returns>
+        IList<string> GetSentences(string content);
+
+        /// <summary>
+        /// Gets the given word occurrence from the content
+        /// </summary>
+        /// <param name="content">The content to count words from</param>
+        /// <param name="word">The word to count the occurrence</param>
+        /// <returns>The word occurrence count</returns>
+        int GetWordCount(string content, string word);
+
+        /// <summary>
+        /// Checks if given word starts with capital letter
+        /// </summary>
+        /// <param name="word">The word to check</param>
+        /// <returns>True if given word starts with capital letter, otherwise false</returns>
+        bool IsCapitalWord(string word);
+
+        /// <summary>
+        /// Checks if given word is in lower
+        /// </summary>
+        /// <param name="word"></param>
+        /// <returns>True if given word is in lower, otherwise false</returns>
+        bool IsLowerWord(string word);
+
+        /// <summary>
+        /// Gets the given complex word occurrence from the content
+        /// </summary>
+        /// <param name="word"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        int GetComplexWordsCount(string content, int length);
+
+        /// <summary>
+        /// Checks if given word is complex
+        /// </summary>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        bool IsComplexWord(string word);
+    }
+}
---
+++ Training.Medium.Sandbox/ContentQualitySection/Services/SpellCheckService.cs
@@ -0,0 +1,67 @@
+﻿using ContentQualitySection.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentQualitySection.Services
+{
+    public class SpellCheckService : ISpellCheckService
+    {
+        public int GetComplexWordsCount(string content, int length)
+        {
+            if (length < 0)
+            {
+                return 0;
+            }
+
+            return GetWords(content).Count(word => word.Length >= length);
+        }
+
+        public IList<string> GetSentences(string content)
+        {
+            return content.Split('.', '!', '?')
+                .Where(sentence => !string.IsNullOrWhiteSpace(sentence)).ToList();
+        }
+
+        public IList<string> GetSplitWords(string content)
+        {
+            return content.Split(' ', '.', ',', '?', '!');
+        }
+
+        public int GetWordCount(string content, string wordForSearch)
+        {
+            return GetWords(content).Count(word => word.Equals(wordForSearch, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public IList<string> GetWords(string content)
+        {
+            return GetSplitWords(content)
+                .Where(word => !string.IsNullOrWhiteSpace(word)).ToList();
+        }
+
+        public bool IsCapitalWord(string word)
+        {
+            if (string.IsNullOrWhiteSpace(word))
+                return false;
+
+            string formattedWord = string.Concat(
+                word.Substring(0, 1).ToUpper(),
+                word.Substring(1).ToLower());
+
+            return word == formattedWord;
+        }
+
+        public bool IsComplexWord(string word)
+        {
+            //Words list berilsa o'sha list ichida borligi tekshiriladi
+            return string.IsNullOrWhiteSpace(word) ? false : word.Length > 20;
+        }
+
+        public bool IsLowerWord(string word)
+        {
+            return word == word.ToLower();
+        }
+    }
+}
