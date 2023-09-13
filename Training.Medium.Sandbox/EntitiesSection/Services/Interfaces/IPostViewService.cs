5bf1485ad979554477738832721f9ba7a922a327
Max <sultonbek.rakhimov@gmail.com>
2023-09-13 23:46:55 +0500

    Add post view interface definition

---
+++ Training.Medium.Sandbox/EntitiesSection/Services/Interfaces/IPostViewService.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+using Shared.Models.Entities;
+
+namespace EntitiesSection.Services.Interfaces;
+
+public interface IPostViewService
+{
+    IQueryable<PostView> Get(Expression<Func<User, bool>> predicate);
+
+    ValueTask<ICollection<PostView>> GetAsync(IEnumerable<Guid> ids);
+
+    ValueTask<ICollection<PostView>> GetByPostId(Guid id);
+
+    ValueTask<ICollection<PostView>> GetByUserId(Guid id);
+
+    ValueTask<PostView?> GetByIdAsync(Guid id);
+
+    ValueTask<PostView> CreateAsync(Guid postId, Guid userId, bool saveChanges = true);
+
+    ValueTask<PostView> DeleteAsync(Guid id, bool saveChanges = true);
+}
---
+++ Training.Medium.Sandbox/Shared/Models/Entities/PostView.cs
@@ -0,0 +1,9 @@
+﻿using Shared.Models.Common;
+
+namespace Shared.Models.Entities;
+
+public class PostView : SoftDeletedEntity
+{
+    public Guid UserId { get; set; }
+    public Guid PostId { get; set; }
+}
