9d7c11bbe6e548aa29726535977604b615b8bbe8
Max <sultonbek.rakhimov@gmail.com>
2023-09-13 23:40:07 +0500

    Update saved post service interface

--- Training.Medium.Sandbox/Shared/Models/Entities/SavedPost.cs
+++ Training.Medium.Sandbox/Shared/Models/Entities/SavedPost.cs
@@ -5,17 +5,5 @@ namespace Shared.Models.Entities;
 
 public class SavedPost : SoftDeletedEntity
 {
-    IQueryable<User> Get(Expression<Func<User, bool>> predicate);
 
-    ValueTask<ICollection<User>> GetAsync(IEnumerable<Guid> ids);
-
-    ValueTask<User?> GetByIdAsync(Guid id);
-
-    ValueTask<User> CreateAsync(User user, bool saveChanges = true);
-
-    ValueTask<User> UpdateAsync(User user, bool saveChanges = true);
-
-    ValueTask<User> DeleteAsync(Guid id, bool saveChanges = true);
-
-    ValueTask<User> DeleteAsync(User user, bool saveChanges = true);
 }
