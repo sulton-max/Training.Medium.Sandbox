757e01e3413fae947d9a8ab557e01c8e0c64dfd4
Ilxom <ilxomkarimjonov22@gmail.com>
2023-09-14 18:21:41 +0500

    Create UserCredentials Model

--- Training.Medium.Sandbox/Shared/DataAccess/Contexts/AppFileContext.cs
+++ Training.Medium.Sandbox/Shared/DataAccess/Contexts/AppFileContext.cs
@@ -10,6 +10,7 @@ public abstract class AppFileContext
 {
     public readonly IFileSet<User, Guid> Users;
     public readonly IFileSet<BlogPost, Guid> Posts;
+    public readonly IFileSet<UserCredentials, Guid> UserCredentials;
 
     public AppFileContext(string folderPath)
     {
@@ -18,17 +19,20 @@ public abstract class AppFileContext
 
         Users = new FileSet<User, Guid>(folderPath, serializer, provider);
         Posts = new FileSet<BlogPost, Guid>(folderPath, serializer, provider);
+        UserCredentials = new FileSet<UserCredentials, Guid>(folderPath, serializer, provider);
     }
 
     public async ValueTask FetchAsync()
     {
         await Users.FetchAsync();
         await Posts.FetchAsync();
+        await UserCredentials.FetchAsync();
     }
 
     public async ValueTask SaveChangesAsync()
     {
         await Users.SaveChangesAsync();
         await Posts.SaveChangesAsync();
+        await UserCredentials.SaveChangesAsync();
     }
 }
