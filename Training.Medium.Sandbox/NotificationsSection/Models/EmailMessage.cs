673d6c471c9fff2dc5d1718361429dfd360eab82
Jasurbek1223 <jasurabdulxaev@gmail.com>
2023-09-14 22:45:47 +0500

    Add Email Massage Model

--- Training.Medium.Sandbox/NotificationsSection/Models/EmailMessage.cs
+++ Training.Medium.Sandbox/NotificationsSection/Models/EmailMessage.cs
@@ -2,5 +2,11 @@
 
 public class EmailMessage
 {
-    
+    public Guid Id { get; set; }
+    public string Subject { get; set; }
+    public string Body { get; set; }
+    public string FromEmailAddress { get; set; }
+    public string ToEmailAddress { get; set; }
+    public bool IsEmailConfirmed { get; set; }
+    public DateTime SendedTime { get; set; }
 }
