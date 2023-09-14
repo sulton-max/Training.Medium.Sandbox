bbc5657b1e76d40ab289257fe19f35ad760560a8
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-09-14 19:05:14 +0500

    Add EmailTemplate

---
+++ Training.Medium.Sandbox/EntitiesSection/Services/Interfaces/IEmailTemplateService.cs
@@ -0,0 +1,15 @@
+﻿using Shared.Models.Entities;
+using System.Linq.Expressions;
+
+namespace EntitiesSection.Services.Interfaces;
+
+public interface IEmailTemplateService
+{
+    IQueryable<EmailTemplate> Get(Expression<Func<EmailTemplate, bool>> expression);
+    ValueTask<ICollection<EmailTemplate>> GetAsync(IEnumerable<Guid> ids);
+    ValueTask<EmailTemplate?> GetByIdAsync(Guid id);
+    ValueTask<EmailTemplate> CreateAsync(EmailTemplate emailTemplate,bool saveChanges = true);
+    ValueTask<EmailTemplate> UpdateAsync(EmailTemplate emailTemplate,bool saveChanges = true);
+    ValueTask<EmailTemplate> DeleteAsync(Guid id,bool saveChanges = true);
+    ValueTask<EmailTemplate> DeleteAsync(EmailTemplate emailTemplate,bool saveChanges = true);
+}
--- Training.Medium.Sandbox/Shared/DataAccess/Contexts/AppFileContext.cs
+++ Training.Medium.Sandbox/Shared/DataAccess/Contexts/AppFileContext.cs
@@ -10,6 +10,7 @@ public abstract class AppFileContext
 {
     public readonly IFileSet<User, Guid> Users;
     public readonly IFileSet<BlogPost, Guid> Posts;
+    public readonly IFileSet<EmailTemplate, Guid> EmailTemplates;
 
     public AppFileContext(string folderPath)
     {
@@ -18,17 +19,20 @@ public abstract class AppFileContext
 
         Users = new FileSet<User, Guid>(folderPath, serializer, provider);
         Posts = new FileSet<BlogPost, Guid>(folderPath, serializer, provider);
+        EmailTemplates = new FileSet<EmailTemplate, Guid>(folderPath, serializer, provider);
     }
 
     public async ValueTask FetchAsync()
     {
         await Users.FetchAsync();
         await Posts.FetchAsync();
+        await EmailTemplates.FetchAsync();
     }
 
     public async ValueTask SaveChangesAsync()
     {
         await Users.SaveChangesAsync();
         await Posts.SaveChangesAsync();
+        await EmailTemplates.SaveChangesAsync();
     }
 }
