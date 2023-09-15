e9608e55d2d349b42c9a300407e4bd913b80a7a8
Abdura <abdura52.uz@gmail.com>
2023-09-14 22:13:58 -0700

    Update ContentQualityService

---
+++ Training.Medium.Sandbox/ContentQualitySection/Services/ContentQualityService.cs
@@ -0,0 +1,54 @@
+﻿using ContentQualitySection.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentQualitySection.Services
+{
+    public class ContentQualityService : IContentQualityService
+    {
+        private ISpellCheckService _spellcheckService;
+
+        public ContentQualityService()
+        {
+            _spellcheckService = new SpellCheckService();
+        }
+
+        public Task<double> CalculateReadTimeAsync(string content)
+        {
+            return Task.Run(() =>
+            {
+                var wordsPerMinute = 200D;
+                var wordsCount = _spellcheckService.GetWords(content).Count;
+                return wordsCount / wordsPerMinute;
+            });
+        }
+
+        public Task<int> CheckCapitalizeWordsAsync(string content)
+        {
+            //do something
+        }
+
+        public Task<int> CheckLowerWordsAsync(string content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> CheckComplexWordsCountAsync(string content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> GetWordOccurrenceCountAsync(string content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> AnalyzeAsync(string content)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Training.Medium.Sandbox/ContentQualitySection/Services/Interfaces/IContentQualityService.cs
@@ -0,0 +1,11 @@
+﻿namespace ContentQualitySection.Services.Interfaces;
+
+public interface IContentQualityService
+{
+    Task<decimal> CalculateReadTimeAsync(string content);
+    Task<int> CheckCapitalizeWordsAsync(string content);
+    Task<int> CheckLowerWordsAsync(string content);
+    Task<int> CheckComplexWordsCountAsync(string content);
+    Task<int> GetWordOccurrenceCountAsync(string content);
+    Task<int> AnalyzeAsync(string content);
+}
